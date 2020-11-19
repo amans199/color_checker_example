918b72208bcdde36ea48bf1200b7c2aa25a8659b
amans199 <ahmed.ouda1997@gmail.com>
2020-11-19 18:44:13 +0200

    #trying the optimize_text_color feature in color_checker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {checkColor} from 'the-simplest-color-checker'
+import { Component } from 'react';
+
+class App extends Component {
+  // constructor(props){
+  //   checkColor({
+  //     selector: ".colors_box", //add any selector
+  //     optimize_text_color: true, //change text color depending on the background
+  //   })
+  // }
+  // componentDidUpdate(){
+  //   checkColor({
+  //        selector: ".colors_box", //add any selector
+  //        optimize_text_color: true, //change text color depending on the background
+  //      })
+  //     }
+  componentDidMount(){
+    checkColor({
+      selector: ".colors_box", //add any selector
+      optimize_text_color: true, //change text color depending on the background
+    })
+  }
+
+      render() {
+      
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+          <div className="boxes">
+            <span data-color="#000" className="colors_box">color box</span>
+              <span data-color="#fff" className="colors_box">color box</span>
+              <span data-color="#0f0" className="colors_box">color box</span>
+              <span data-color="#00f" className="colors_box">color box</span>
+              <span data-color="#f00" className="colors_box">color box</span>
+          </div>
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        );
+}
 }
 
 export default App;
